5adfb39a23f6a6e25178eda4275fc3c04a109cf8
CristianRR21 <cristian.roblero7197@utc.edu.ec>
2024-11-19 11:45:34 -0500

    Primer commit del proyecto

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+namespace prueba_1_githunb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("este es el mensaje numero uno");
+
+        }
+    }
+}
